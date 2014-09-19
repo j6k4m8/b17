cabbc6c475dd7d371e9e406c1254dba8b5f28ede
Jordan <j6k4m8@gmail.com>
2014-09-19 12:27:18 -0400

    b17 startup

---
+++ b17.py
@@ -0,0 +1,36 @@
+import skimage.io as io
+import skimage.transform as sk_transform
+
+B17_CONST = {
+    "INVALID_IMAGE":          "Invalid image."
+}
+
+class B17Exception(Exception):
+    pass
+
+
+class NeuroImage(io.Image):
+    """skimage-mutant that contains all or part of a neural image."""
+
+    def __init__(self, image):
+        self.image = image
+        if self.image == []:
+            raise B17Exception(B17_CONST.INVALID_IMAGE)
+
+
+    def find_blobs(self):
+        """
+        Convert the image to grayscale, and then threshold the
+        found blob-size (from skimage.blob_...). Return a blob[].
+        """
+
+
+    def re_res(self, proportion):
+        return NeuroImage(
+            sk_transform.resize(
+                self.image,
+                (
+                    int(proportion * self.image.shape[0]),
+                    int(proportion * self.image.shape[1])
+                )
+            ))
